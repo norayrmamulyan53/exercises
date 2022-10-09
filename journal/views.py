a9724360f6351b1ed9d0e00868ab86b0bee4091c
norayr <norayr.software@gmail.com>
2022-10-10 01:20:37 +0400

    Current version

--- journal/views.py
+++ journal/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Teacher, Student
 
-class Info:
-    teachers = Teacher.objects.all()
-    students = Student.objects.all()
 
 def index(request):
     return render(request, 'journal/index.html', {
