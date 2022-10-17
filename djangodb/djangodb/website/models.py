d47bfb0d30e1d4939653840e7b2544be0c57c63c
norayr <norayr.software@gmail.com>
2022-10-17 15:11:04 +0400

    DjangoDB playlist result

--- djangodb/djangodb/website/models.py
+++ djangodb/djangodb/website/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=200)
+    passwd = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
+
+    class Meta:
+        verbose_name = "Member"
+        verbose_name_plural = "Members"
+
+
--- djangodb/djangodb/website/views.py
+++ djangodb/djangodb/website/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Member
+from .forms import MemberForm
+from django.contrib import messages
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    params = {
+        'all': Member.objects.all
+    }
+    return render(request, 'index.html', params)
+
+
+def join(request):
+    if request.method == 'POST':
+        form = MemberForm(request.POST or None)
+        if not form.is_valid():
+            raise Exception(f'form is not valid {form.errors}')
+        form.save()
+        messages.success(request, 'Registration passed successfully')
+        return redirect('index')
+    else:
+        return render(request, 'join.html', {})
